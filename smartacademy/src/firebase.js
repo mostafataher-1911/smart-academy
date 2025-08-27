14ceb93dc9fbd9fe1abfc8150680bfbef974a6cd
mostafataher-1911 <mostafataher339@gmail.com>
2025-08-27 18:54:53 +0200

    update

--- smartacademy/src/firebase.js
+++ smartacademy/src/firebase.js
@@ -1,12 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics} from "firebase/analytics";
+
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { getAnalytics, isSupported } from "firebase/analytics";
+
 
 
 
@@ -22,7 +25,13 @@ const firebaseConfig = {
 
 
 // Initialize Firebase
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) analytics = getAnalytics(app);
+  });
+}
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
