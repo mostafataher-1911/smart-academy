447ccdec2fedfcf398c8c68f76902904fd807700
mostafataher-1911 <mostafataher339@gmail.com>
2025-08-27 19:39:37 +0200

    update 2

--- src/firebase.js
+++ src/firebase.js
@@ -8,29 +8,24 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-import { getAnalytics, isSupported } from "firebase/analytics";
+
 
 
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD2kGvj3FEJotWnPZuG7HDvvUYvcVt-Z0g",
+  authDomain: "smart-academy-760e4.firebaseapp.com",
+  projectId: "smart-academy-760e4",
+  storageBucket: "smart-academy-760e4.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:361654417255:web:5aacb661bf1ca5c73021c2",
+  measurementId: "G-12CFRR7YQ6",
 };
 
 
 // Initialize Firebase
-let analytics;
-if (typeof window !== "undefined") {
-  isSupported().then((yes) => {
-    if (yes) analytics = getAnalytics(app);
-  });
-}
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
